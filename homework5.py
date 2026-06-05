2ae1997a5160bdfea8a01927a90fa0e91f1c2421
roshan <443roshanjp@gmail.com>
2026-06-05 15:38:06 +0900

    homework5 committed

---
+++ homework5.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def center_of_gravity(x):
+    c = np.dot(np.arange(len(x)), x) / np.sum(x)
+    return c
+
+
+def matched_identity(x):
+
+    I = np.eye(len(x))
+    return I
+
+
+def sine_and_cosine(t_start, t_end, t_steps):
+
+    t = np.linspace(t_start, t_end, t_steps)
+    x = np.cos(t)
+    y = np.sin(t)
+
+    return t, x, y
